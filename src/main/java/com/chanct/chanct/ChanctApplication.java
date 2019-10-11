d82a543731df15775eb69badba773944fb1112c4
Sunnyzhao <272060655@qq.com>
2019-10-11 17:16:38 +0800

    init

---
+++ src/main/java/com/chanct/chanct/ChanctApplication.java
@@ -0,0 +1,14 @@
+package com.chanct.chanct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChanctApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ChanctApplication.class, args);
+    }
+
+}
